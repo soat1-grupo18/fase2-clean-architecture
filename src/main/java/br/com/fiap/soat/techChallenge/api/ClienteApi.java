9e3f860d7f1e4ee7d5c8c0ad10308521d5dffbc0
Pablo Lima Dias <pabloidz@gmail.com>
2023-09-01 22:35:01 -0300

    Adiciona descrição para endpoints

--- src/main/java/br/com/fiap/soat/techChallenge/api/ClienteApi.java
+++ src/main/java/br/com/fiap/soat/techChallenge/api/ClienteApi.java
@@ -3,11 +3,14 @@ package br.com.fiap.soat.techChallenge.api;
 import br.com.fiap.soat.techChallenge.controllers.ClienteController;
 import br.com.fiap.soat.techChallenge.presenters.ClientePresenter;
 import br.com.fiap.soat.techChallenge.api.requests.CadastrarClienteRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Tag(name = "API de Clientes")
 public class ClienteApi {
 
     private final ClienteController clienteController;
@@ -16,11 +19,13 @@ public class ClienteApi {
         this.clienteController = clienteController;
     }
 
+    @Operation(summary = "Identificar cliente", description = "Retorna um cliente com o CPF informado.")
     @GetMapping("/clientes/{cpf}")
     public ResponseEntity<ClientePresenter> identificarCliente(@PathVariable String cpf) {
         return ResponseEntity.ok(this.clienteController.identificarCliente(cpf));
     }
 
+    @Operation(summary = "Cadastrar cliente", description = "Cadastra um cliente com os dados informados.")
     @PostMapping("/clientes")
     public ResponseEntity<ClientePresenter> cadastrarCliente(@Valid @RequestBody CadastrarClienteRequest cadastrarClienteRequest) {
         return ResponseEntity.ok(this.clienteController.cadastrarCliente(cadastrarClienteRequest.toDomain()));
--- src/main/java/br/com/fiap/soat/techChallenge/api/PagamentoApi.java
+++ src/main/java/br/com/fiap/soat/techChallenge/api/PagamentoApi.java
@@ -3,12 +3,15 @@ package br.com.fiap.soat.techChallenge.api;
 import br.com.fiap.soat.techChallenge.api.requests.ConfirmacaoPagamentoRequest;
 import br.com.fiap.soat.techChallenge.controllers.PagamentoController;
 import br.com.fiap.soat.techChallenge.presenters.PagamentoPresenter;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
+@Tag(name = "API de Pagamentos")
 public class PagamentoApi {
 
     private final PagamentoController pagamentoController;
@@ -17,11 +20,17 @@ public class PagamentoApi {
         this.pagamentoController = pagamentoController;
     }
 
+    @Operation(summary = "Consultar status de pagamento", description = "Consulta o status de pagamento a partir do id do pedido.")
     @GetMapping("/pagamentos/{pedidoId}")
     public ResponseEntity<PagamentoPresenter> consultarStatusPagamento(@PathVariable UUID pedidoId) {
         return ResponseEntity.ok(this.pagamentoController.consultarStatusPagamento(pedidoId));
     }
 
+    @Operation(summary = "Receber confirmaçào de pagamento (Webhook)",
+            description = "Webhook para receber a confirmação de pagamento enviada pelo MercadoPago.<br>" +
+                    "Somente os parâmetros action e data.id serão processados.<br>" +
+                    "- action: Ação a ser processada. Somente a confirmação de pagamento é aceita no momento. Exemplo: payment.created<br>" +
+                    "- data.id: O id de pagamento registrado no checkout de pedido.")
     @PostMapping("/pagamentos")
     public ResponseEntity<String> receberConfirmacaoPagamento(@RequestBody ConfirmacaoPagamentoRequest confirmacaoPagamentoRequest) {
         return ResponseEntity.ok(this.pagamentoController.receberConfirmacaoPagamento(confirmacaoPagamentoRequest.toDomain()));
